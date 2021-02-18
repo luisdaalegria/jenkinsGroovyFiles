89a06447ff62732e8309aab8880538dbd6255bab
Alegria <luisdaalegria@gmail.com>
2021-02-17 21:49:29 -0600

    Run python script

--- declarativePipeline1.groovy
+++ declarativePipeline1.groovy
@@ -1,12 +1,20 @@
 pipeline {
     agent any
     stages {
-        stage('build') {
+        stage('Clone repo') {
             steps {
                 dir('repo1') 
                 {
                     git url: 'https://github.com/luisdaalegria/jenkinsIntegrationDemo.git',
                     branch: 'master'
+                }
+            }
+        }
+        stage('Run script') {
+            steps {
+                dir('repo1') 
+                {
+                    sh 'python helloWorld.py'
                 }
             }
         }
