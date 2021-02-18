0b1a1b1fdba2138b783152c4d76b1456dff0574d
Alegria <luisdaalegria@gmail.com>
2021-02-17 22:50:48 -0600

    New commit

--- scriptedPipeline1.groovy
+++ scriptedPipeline1.groovy
@@ -1,4 +1,10 @@
 node {
+     properties([
+        parameters([
+            string(name: 'STRING_TO_PRINT_ONE', defaultValue: 'TESTING' )
+            string(name: 'STRING_TO_PRINT_TWO', defaultValue: 'TESTING' )
+        ])
+    ])    
 
     stage('Clone repo') {
         git url: 'https://github.com/luisdaalegria/jenkinsIntegrationDemo.git',
@@ -6,6 +12,14 @@ node {
     }
     stage('Run script') {
         sh 'python helloWorld.py'
+    }
+
+    stage('String to print 1') {
+        println "String to print 1 is: $STRING_TO_PRINT_ONE"
+    }
+
+    stage('String to print 2') {
+        println "String to print 2 is: $STRING_TO_PRINT_TWO"
     }
     
 }
