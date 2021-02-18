fa62b2effb2afc3828bf8c0885f27b224be25e12
Alegria <luisdaalegria@gmail.com>
2021-02-17 22:18:42 -0600

    Scripted pipeline

--- scriptedPipeline1.groovy
+++ scriptedPipeline1.groovy
@@ -1,21 +1,11 @@
 node {
 
     stage('Clone repo') {
-        steps {
-            dir('repo1') 
-            {
-                git url: 'https://github.com/luisdaalegria/jenkinsIntegrationDemo.git',
-                branch: 'master'
-            }
-        }
+        git url: 'https://github.com/luisdaalegria/jenkinsIntegrationDemo.git',
+        branch: 'master'
     }
     stage('Run script') {
-        steps {
-            dir('repo1') 
-            {
-                sh 'python helloWorld.py'
-            }
-        }
+        sh 'python helloWorld.py'
     }
     
 }
